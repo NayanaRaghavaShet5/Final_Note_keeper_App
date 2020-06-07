4935749c565c7eb48afcd49592aad77609522e01
NayanaRaghavaShet5 <51402931+NayanaRaghavaShet5@users.noreply.github.com>
2020-06-07 12:42:44 +0530

    Completed Versions

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import AddCircleRoundedIcon from "@material-ui/icons/AddCircleRounded";
+
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+  const [isExa, setExa] = useState(false);
+
+  function setExp() {
+    setExa(true);
+  }
+
   return (
     <div>
-      <form>
-        <input
-          onChange={e => props.handleNote(e)}
-          name="title"
-          placeholder="Title"
-          value={props.titleValue}
-        />
+      <form className="create-note">
+        {isExa && (
+          <input
+            onChange={e => props.handleNote(e)}
+            name="title"
+            placeholder="Title"
+            value={props.titleValue}
+          />
+        )}
         <textarea
           onChange={e => props.handleNote(e)}
           name="content"
           placeholder="Take a note..."
-          rows="5"
+          rows={isExa ? 5 : 1}
           value={props.contentValue}
+          onClick={setExp}
         />
-        <button onClick={props.addNote}>Add</button>
+        <Zoom in={isExa}>
+          <Fab onClick={props.addNote}>
+            <AddCircleRoundedIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
